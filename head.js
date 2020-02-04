f6c08a2efad6dbc58d8744f9997471045d972511
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-04 00:22:34 +0000

    added the head function

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(givenArray){
+  return givenArray[0];
+};
+
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
