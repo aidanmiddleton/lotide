3794b4fef50e9716ec9b00e484feae1ae69f5a3b
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-06 23:54:37 +0000

    added more test conditions to map function

--- map.js
+++ map.js
@@ -34,10 +34,10 @@ const map = function(array, callback) {
 const results1 = map(words, word => word.length);
 console.log(results1);
 
-console.log(eqArrays(['kill me'], ['kill me']))
 assertArraysEqual([1, 2], [1, 2])
-//console.log(assertArraysEqual(([1, 2], [1, 2])))
-assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3])
-//assertArraysEqual((map(words, word => word.length), [6, 7, 2, 5, 3]))
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map(words, word => word.toUpperCase()), ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
+
 
 
