d01541df194c677bc3f4d17c18439dc9c7f76a39
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-06 23:50:46 +0000

    made changes to the assertArraysEqual function

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,22 +1,21 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let isEqual;
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) {
-      isEqual = true;
-    } else {
-      isEqual = false;
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  return isEqual;
+  return true;
 };
 
 const assertArraysEqual = function(firstA, secondA) {
-  if ((eqArrays(firstA, secondA)) === true) {
+  if (eqArrays(firstA, secondA)) {
     console.log(`Assertion Passed: ${firstA} === ${secondA}. Well done!😍`);
-  } else if ((eqArrays(firstA, secondA)) === false) {
+  } else {
     console.log(`Assertion failed: ${firstA} !== ${secondA}. you were sadly mistaken🤬`);
   }
 };
-
 assertArraysEqual([1, 2, 4], [1, 2, 5])
 
---
+++ map.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstA, secondA) {
+  if (eqArrays(firstA, secondA)) {
+    console.log(`Assertion Passed: ${firstA} === ${secondA}. Well done!😍`);
+  } else {
+    console.log(`Assertion failed: ${firstA} !== ${secondA}. you were sadly mistaken🤬`);
+  }
+};
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results;
+}
+
+const results1 = map(words, word => word.length);
+console.log(results1);
+
+console.log(eqArrays(['kill me'], ['kill me']))
+assertArraysEqual([1, 2], [1, 2])
+//console.log(assertArraysEqual(([1, 2], [1, 2])))
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3])
+//assertArraysEqual((map(words, word => word.length), [6, 7, 2, 5, 3]))
+
+
