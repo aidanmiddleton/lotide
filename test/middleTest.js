22c7da0aafc46374280ae1750234cdfbbab3aa32
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-11 00:46:36 +0000

    updated middle and tail tests to use chai assertions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArrayEqual')
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+
+describe("#middle", () => {
+
+  it ('should return [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  
+  it ('should return [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3,4])
+  });
+
+  it ('should return [] for [1]', () => {
+    assert.deepEqual(middle([1]), [])
+  })
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,20 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
 
 
-const words = ['thing 1', 'thing 2', 'thing 3']
-assertEqual(tail(words).length, 3);
-assertEqual(tail([1, 2, 3, 4, 5, 6]).length, 5)
+// const words = ['thing 1', 'thing 2', 'thing 3']
+// assertEqual(tail(words).length, 3);
+// assertEqual(tail([1, 2, 3, 4, 5, 6]).length, 5)
+
+describe("#tail", () => {
+
+  it ('should return [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  
+  it ('should return [1] for [2, 1]', () => {
+    assert.deepEqual(tail([2, 1]), [1])
+  })
+
+});
